1f81bfc05f021e8e4b692cd613ae9886a27627d0
Hubert <hubertsosnowski122@gmail.com>
2023-09-07 09:29:25 +0200

    Better display of accountants

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../UI/Button";
+import { styled } from "styled-components";
+
+const LinkStyled = styled(Link)`
+  text-decoration: none;
+`
 
 const Home = () => {
   return (
     <div>
-      <Link to="/ksiegowi"><Button text="Księgowi" /></Link>
+      <LinkStyled to="ksiegowi">
+        <Button text="Księgowi" />
+      </LinkStyled>
     </div>
   )
 }
